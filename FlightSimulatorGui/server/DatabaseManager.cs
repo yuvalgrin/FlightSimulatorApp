1c13f4b07eed60b6206543e95647945cd4dbf8c8
yuvalgrin <juvs2k@gmail.com>
2020-03-18 04:20:32 -0700

    Basic design for backend

---
+++ FlightSimulatorGui/server/DatabaseManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightSimulatorGui.server
+{
+    // Hold the data from FS
+    // Hold and update queue of queries to be sent
+    // Get updates from FS into the data map
+    class DatabaseManager
+    {
+        public Queue<string> getCommandsQueue() { return null; }
+
+        // If set command had value more than max (same for less than min) put the closest valid value
+        public void addCommandToQueue() { return; }
+
+        // before sending to queue
+        public string createSetCommand(string type, string value) { return null; }
+
+
+        // If value is error (equals ERR) throw exception?
+        public string getDataByKey(string key) { return null; }
+        public void putDataByKey(string key, string value) { return; }
+
+    }
+}
---
+++ FlightSimulatorGui/server/FSConnector.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightSimulatorGui.server
+{
+    // Runs a thread that accesses the FS and:
+    // 1. Executes the command from DB manager queue
+    // 2. Read data from server and update DB manager map
+    class FSConnector
+    {
+        public static void initlizeBackgroundThread() { }
+
+        public void runCommandToFS() { }
+
+        public void updateDataFromFS() { }
+
+    }
+}
