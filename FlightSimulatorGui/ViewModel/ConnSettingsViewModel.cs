d5152dbc065899df9b5fdd57f77959a3768cfe93
yuvalgrin <juvs2k@gmail.com>
2020-04-01 03:48:37 -0700

    conn settings added

---
+++ FlightSimulatorGui/ViewModel/ConnSettingsViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightSimulatorGui.ViewModel
+{
+    class ConnSettingsViewModel : BaseNotify
+    {
+        String VM_ConnRes
+        {
+            set
+            {
+                NotifyPropertyChanged("VM_ConnRes");
+            }
+        }
+
+        public void queryUpdate(String ip, String port)
+        {
+            VM_ConnRes = "Loading...";
+            //model.executeCtrlRoomQuery(ip, port);
+        }
+    }
+}
---
+++ FlightSimulatorGui/Views/ConnSettings.xaml.cs
@@ -0,0 +1,37 @@
+﻿using FlightSimulatorGui.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FlightSimulatorGui.Views
+{
+    /// <summary>
+    /// Interaction logic for FSDataView.xaml
+    /// </summary>
+    public partial class ConnSettings : UserControl
+    {
+        ConnSettingsViewModel connSettingsViewModel;
+
+        public ConnSettings()
+        {
+            InitializeComponent();
+            connSettingsViewModel = new ConnSettingsViewModel();
+        }
+
+        private void btnData_Click(object sender, RoutedEventArgs e)
+        {
+            connSettingsViewModel.queryUpdate(tbInput.Text);
+        }
+    }
+}
